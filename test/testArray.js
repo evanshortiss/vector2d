b80f1898c3d8dc46e6aab6a9a68d3c0f0c555740
Evan Shortiss <evanshortiss@gmail.com>
2014-05-17 23:54:30 +0100

    Fix tests

--- test/testArray.js
+++ test/testArray.js
@@ -4,7 +4,7 @@
  */
 
 var assert = require('assert'),
-  Vec2D = require('../src/Vec2d.js');
+  Vec2D = require('../src/Vec2D.js');
 
 describe('Test Vec2D Library instance methods.', function() {
 
