9d72db59e892240a611c4eac2ff887a59d33cbbe
Evan Shortiss <evanshortiss@gmail.com>
2023-10-31 10:15:48 -0700

    fix: disable trailing comma in prettier

--- src/AbstractVector.ts
+++ src/AbstractVector.ts
@@ -4,7 +4,7 @@
  */
 const precision = [
   1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
-  10000000000,
+  10000000000
 ]
 
 export interface VectorConstructable<T> {
@@ -104,7 +104,7 @@ export abstract class AbstractVector {
   toObject() {
     return {
       x: this.x,
-      y: this.y,
+      y: this.y
     }
   }
 
